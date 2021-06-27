4222d748059a70144a824038e3cce4dcdc1cf04b
Akira <smxshxishxad@me.com>
2021-06-27 21:43:37 +0900

    :pencil1: Fix typo

--- private_diary/settings_common.py
+++ private_diary/settings_common.py
@@ -22,7 +22,6 @@ except ImportError:
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -190,7 +189,7 @@ SITE_ID = 1
 
 AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
-    'django.contrib.auth.backends.ModelBacend',
+    'django.contrib.auth.backends.ModelBackend',
 )
 
 ACCOUNT_AUTHETICATION_METHOD = 'email'
